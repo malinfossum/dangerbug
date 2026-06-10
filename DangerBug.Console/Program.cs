936f04e37a66bbe97ea4b2fd3a2668c52c816622
andreasiversen98 <andreasiversen98@protonmail.com>
2026-06-10 13:01:02 +0200

    Add bug list and add functionality; stub App class
    Implemented ShowList and AddBug methods in Program.cs to display and add bugs via the menu. Updated menu logic to use these methods. Added App.cs with a basic App class definition and required usings.

--- DangerBug.Console/Program.cs
+++ DangerBug.Console/Program.cs
@@ -34,16 +34,67 @@ internal static class Program
             switch (choice)
             {
                 case "1":
-
-
+                    ShowList(bugs);
+                    break;
+                case "2":
+                    AddBug(bugs);
+                    break;
+                case "0":
+                    running = false;
+                    break;
                 default:
                     WriteLine("Invalid choice, try again");
                     break;
             }
         }
     }
+
     private static void ShowList(List<Bug> bugs)
     {
+        WriteLine("\nBugs");
+        for (int i = 0; i < bugs.Count; i++)
+        {
+            WriteLine($"{i + 1}. {bugs[i].Name}");
+        }
+
+        Write("Pick a number for bug details");
+        string input = ReadLine() ?? "";
+        if (int.TryParse(input, out int number) && number >= 1 && number <= bugs.Count)
+        {
+            WriteLine();
+            Write(bugs[number - 1].Describe());
+        }
+        else if (input != "")
+
+        {
+            WriteLine("No bug with that number");
+        }
 
     }
+
+    private static void AddBug(List<Bug> bugs)
+    {
+        Write("Bug name: ");
+        string name = ReadLine() ?? "";
+        if (name == "")
+        {
+            WriteLine("Name can't be empty.");
+            return;
+        }
+
+        Bug bug = new Bug(name);
+
+        Write("Can it fly? (y/n): ");
+        bug.CanFly = (ReadLine() ?? "") == "y";
+
+        Write("Can it bite? (y/n): ");
+        bug.CanBite = (ReadLine() ?? "") == "y";
+
+        Write("What plague does it cause? (Enter for none): ");
+        bug.Plague = ReadLine() ?? "";
+
+        bugs.Add(bug);
+        WriteLine($"{name} added!");
+    }
+
 }
---
+++ DangerBug.Core/Models/App.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DangerBug.Core.Models;
+
+internal class App
+{
+
+}
