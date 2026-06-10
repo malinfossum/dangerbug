9990d59c8131973a9ddcec5b2a98ba2b6e204d4f
Malin Fossum <malinfossum.dev@proton.me>
2026-06-10 11:39:49 +0200

    feat: implement bug model, console menu scaffolded

--- DangerBug.Console/Program.cs
+++ DangerBug.Console/Program.cs
@@ -1,11 +1,49 @@
+using static System.Console;
+
+using DangerBug.Core.Models;
+
 namespace DangerBug.Console;
+
+// TODO: build the menu loop.
+//   1. List bugs   2. Add a bug   0. Exit
 
 internal static class Program
 {
     private static void Main()
     {
-        // TODO: build the menu loop.
-        //   1. List bugs   2. Add a bug   0. Exit
-        // The bug types live in DangerBug.Core.Models.
+        List<Bug> bugs = new List<Bug>
+        {
+            new Mosquito(),
+            new Housefly(),
+            new Spider(),
+            new Tick(),
+            new Wasp(),
+        };
+
+        bool running = true;
+        while (running)
+        {
+            WriteLine("\n--- DangerBug ---");
+            WriteLine("1. List bugs");
+            WriteLine("2. Add a bug");
+            WriteLine("0. Exit");
+            Write("Choice: ");
+
+            string choice = ReadLine() ?? "";
+
+            switch (choice)
+            {
+                case "1":
+
+
+                default:
+                    WriteLine("Invalid choice, try again");
+                    break;
+            }
+        }
+    }
+    private static void ShowList(List<Bug> bugs)
+    {
+
     }
 }
